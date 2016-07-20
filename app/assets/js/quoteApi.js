1818ca7bfc4b29991917fffc2debb3012599d2c6
r00t1000 <jacobjimenez@Jacobs-MacBook-Pro.local>
2016-07-19 18:43:11 -0700

    Done! ReactJs pulls data finally

---
+++ app/assets/js/quoteApi.js
@@ -0,0 +1,48 @@
+
+var QuoteApi = React.createClass({
+
+    getInitialState: function () {
+        return {
+            quote: "",
+            author: ""
+        };
+    },
+
+    componentWillMount: function () {
+        $.ajax({
+            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies',
+            dataType: 'json',
+            method: 'POST',
+            headers: {
+                'X-Mashape-Key': '#'
+            },
+            cache: false,
+            success: function (data) {
+                this.setState({
+                    quote: data.quote,
+                    author: data.author
+                });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                this.setState({
+                    error: err,
+                    status: status
+                });
+            }.bind(this)
+        });
+    },
+
+    render: function () {
+
+        return (
+            <div>
+                <center><h3>{this.state.quote}</h3></center>
+                <p className="source-text"><strong>Movie: </strong> {this.state.author}</p>
+            </div >
+        );
+    }
+});
+ReactDOM.render(
+    <QuoteApi />,
+    document.getElementById('app')
+);
